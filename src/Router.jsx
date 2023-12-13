6d563e1062d600f1b4c588170a0deb2faa9bc5e6
Julio <jcsmerubia@gmail.com>
2023-12-13 12:28:18 +0100

    routes

---
+++ src/Router.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes} from "react-router-dom";
+import Home from './pages/Home'
+import About from './components/about'
+import Events from './components/events'
+import Music from './components/music'
+import Art from './components/art'
+import Press from './components/press' 
+import Collaborations from './components/collaboration'
+
+function Router(){
+    return(
+        <Routes>
+            <Route path='/' element={Home} />
+            <Route path='/about' element={About} />
+            <Route path='/events' element={Events} />
+            <Route path='/music' element={Music} />
+            <Route path='/art' element={Art} />
+            <Route path='/press' element={Press} />
+            <Route path='/collaborations' element={Collaborations} />
+        </Routes>
+    )
+}
+
+export default Router;
