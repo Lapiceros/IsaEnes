f0f53bec8aa65de8f6145fe70915eef31fb22697
isaiasd2712 <isaiasd2712@gmail.com>
2023-12-18 13:29:21 +0100

    card

--- src/components/press/Press.jsx
+++ src/components/press/Press.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Press.css'
+import pressImage from '../../assets/img/imagesIsa/isa2.png'
+import perfil from '../../assets/img/imagesIsa/isa.png'
 
 function Press() {
   return (
-    <h1>Isa Enes</h1>
-    
+    <section className='press'>
+      <div className='pressCard'>
+        <img src={pressImage} alt='articulo de prensa' className='imageCard'></img>
+        <img src={perfil} alt='articulo de prensa' className='perfilImg'></img>
+        <div className="eventText">
+          <h4>Isa Penes</h4>
+          <p> Isa Penes Come muchos penes oh waos <br />
+            ¿quieres echar un vistazo de cuantos penes come isa?</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
