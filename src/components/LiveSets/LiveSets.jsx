84de8ce86940af23e4cb6c181350b09b9bcffc32
limematheuz <mat.rib.lima@gmail.com>
2023-12-19 09:53:50 +0100

    swiper added to livesets

--- src/components/LiveSets/LiveSets.jsx
+++ src/components/LiveSets/LiveSets.jsx
@@ -1,27 +1,55 @@
-import React from "react";
-import Swiper from "../../components/swiper/Swiper";
+import React from 'react';
 import "./LiveSets.css"
 
-import albumImage1 from "../../assets/img/liveSetsimg/album1.jpeg";
-import albumImage2 from "../../assets/img/liveSetsimg/album2.jpeg";
-import albumImage3 from "../../assets/img/liveSetsimg/album3.jpeg";
-import albumImage4 from "../../assets/img/liveSetsimg/album4.jpeg";
-import albumImage5 from "../../assets/img/liveSetsimg/album5.jpeg";
-import albumImage6 from "../../assets/img/liveSetsimg/album6.jpeg";
+import album1 from "../../assets/liveSetsimg/album1.jpeg";
+import album2 from "../../assets/liveSetsimg/album2.jpeg";
+import album3 from "../../assets/liveSetsimg/album3.jpeg";
+import album4 from "../../assets/liveSetsimg/album4.jpeg";
+import album5 from "../../assets/liveSetsimg/album5.jpeg";
+import album6 from "../../assets/liveSetsimg/album6.jpeg";
 
-export default function MusicProductions() {
-  const images = [
-    { src: albumImage1, className: "gallery-item gallery-item-1" },
-    { src: albumImage2, className: "gallery-item gallery-item-2" },
-    { src: albumImage3, className: "gallery-item gallery-item-3" },
-    { src: albumImage4, className: "gallery-item gallery-item-4" },
-    { src: albumImage5, className: "gallery-item gallery-item-5" },
-    { src: albumImage6, className: "gallery-item gallery-item-6" },
-  ];
 
+export default function LiveSets() {
   return (
-    <div>
-      <Swiper images={images} />
+    <div className='carousel-container'>      
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src={album1} alt="First slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={album2} alt="Second slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={album3} alt="Third slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={album4} alt="Third slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={album5} alt="Third slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src={album6} alt="Third slide" />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/MusicProductions/MusicProductions.jsx
+++ src/components/MusicProductions/MusicProductions.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import Swiper from "../../components/swiper/Swiper";
+import React from 'react';
 
-import isaimage1 from "../../assets/img/musicProductionsimg/isasproduction.png";
-import isaimage2 from "../../assets/img/musicProductionsimg/isasproduction2.png";
-import isaimage3 from "../../assets/img/musicProductionsimg/isasproduction3.png";
 
 export default function MusicProductions() {
-  const images = [isaimage1, isaimage2, isaimage3];
   return (
     <div>
-      <Swiper images={images} />
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="ruta-de-la-primera-imagen.jpg" alt="First slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="ruta-de-la-segunda-imagen.jpg" alt="Second slide" />
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="ruta-de-la-tercera-imagen.jpg" alt="Third slide" />
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
     </div>
   );
 }
