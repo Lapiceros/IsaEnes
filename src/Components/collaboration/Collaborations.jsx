cb983cb47f3646a051cb4de96c80ddbdd5dea48c
limematheuz <mat.rib.lima@gmail.com>
2023-12-28 14:02:14 +0100

    imagesgallery/ responsive/ menu fixed

--- src/Components/collaboration/Collaborations.jsx
+++ src/Components/collaboration/Collaborations.jsx
@@ -7,12 +7,14 @@ import collaborations3 from "../../assets/collaborationsimg/collaborations3.png"
 import Carrousel from '../carrousel/Carrousel';
 
 export default function Collaborations() {
+  const title = ["COLLABORATIONS"];
   const images = [collaborations1, collaborations2 , collaborations3];
   const imageLinks = [];
+  const captions = ["DESCRIPTION", "DESCRIPTION", "DESCRIPTION"];
 
   return (
     <div>
-      <Carrousel images={images} imageLinks={imageLinks} />
+      <Carrousel title={title} images={images} imageLinks={imageLinks} captions={captions} />
     </div>
   );
 }
