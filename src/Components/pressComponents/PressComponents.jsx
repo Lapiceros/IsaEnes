c126cf22910533cda7fcf42dea95bd95eaf356cc
isaiasd2712 <isaiasd2712@gmail.com>
2024-01-02 09:58:13 +0100

    pantalla chiquita

--- src/Components/pressComponents/PressComponents.jsx
+++ src/Components/pressComponents/PressComponents.jsx
@@ -8,13 +8,13 @@ function PressComponents() {
       {data.map((item) => (
         <section key={item.id} className='press'>
           <div className='pressData'>
-            <h5 className='Header'>{item.Nombre}</h5>
+            <h5 className='header'>{item.Nombre}</h5>
             <div className="imageContainer">
               <img src={item.imgUrl} alt='articulo de prensa' className='imagePress'></img>
               <div className="overlay">
                 <div className="eventText">
                   <h4 className='title'>{item.Título}</h4>
-                  <p>{item.Reseña}</p>
+                  <p className='review'>{item.Reseña}</p>
                 </div>
               </div>
             </div>
